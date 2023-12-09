ac3501e1754fd3b283486aeb4ad9fd39eff34abb
Animesh478 <animeshmohanty.am@gmail.com>
2023-12-10 00:53:35 +0530

    anagram done

--- Week-1/01-js/easy/anagram.js
+++ Week-1/01-js/easy/anagram.js
@@ -5,7 +5,17 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.replace(' ', '').toLowerCase();
+  str2 = str2.replace(' ', '').toLowerCase();
 
+  str1Sorted = str1.split('').sort().join('');
+  str2Sorted = str2.split('').sort().join('');
+
+  if(str1Sorted === str2Sorted){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 module.exports = isAnagram;
