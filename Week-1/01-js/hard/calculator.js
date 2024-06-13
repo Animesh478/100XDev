bf1e1a0e2a7b404655ac1b0daba1672b7b9a2b3b
Animesh478 <animeshmohanty.am@gmail.com>
2024-06-13 19:07:04 +0530

    Some problem with the calculate(). Could not pass all the tests

--- Week-1/01-js/hard/calculator.js
+++ Week-1/01-js/hard/calculator.js
@@ -16,6 +16,65 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+const math = require("mathjs");
+
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+  add(input) {
+    this.result = this.result + input;
+    return this;
+  }
+  subtract(input) {
+    this.result = this.result - input;
+    return this;
+  }
+  multiply(input) {
+    this.result *= input;
+    this.result.toFixed(2);
+    return this;
+  }
+  divide(input) {
+    if (input !== 0) {
+      this.result /= input;
+      this.result.toFixed(2);
+      return this;
+    } else {
+      throw new Error("Not valid");
+    }
+  }
+  clear() {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result;
+  }
+  calculate(str) {
+    const exp = str.replace(/\s/g, "");
+    const regex = /[a-zA-Z]/;
+    if (regex.test(exp)) {
+      throw new Error("Invalid expression");
+    } else if (exp.includes("/0")) {
+      throw new Error("Cannot divide by 0");
+    } else {
+      return Number(math.evaluate(exp).toFixed(2));
+    }
+  }
+}
+
+const x = new Calculator();
+console.log(x.calculate("10 +   2 *    (   6 - (4 + 1) / 2) + 7"));
+console.log(x.calculate("   (2.5 + 1.5) * 3 "));
+console.log(x.calculate("   10/3"));
+// console.log(x.calculate("   10/0"));
+// console.log(x.divide(2));
+// console.log(x.multiply(-2));
+// console.log(x.add(-2));
+x.subtract(5);
+x.subtract(3);
+console.log(x.getResult());
+console.log(x.calculate("2 + 3 * 4"));
+console.log("hi there");
 
 module.exports = Calculator;
